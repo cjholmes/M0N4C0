5e2cd004d494767c11d2585fb8f807d51fb5783c
cmakhilan <cm.akhilan@gmail.com>
2024-12-27 16:08:42 -0500

    weighted graph implemented

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-import numpy as np 
 
-# Initializing Hashmap of all datat points 
-testdict  = {
-    "AB"  : 0.1,
-    "AC" : 10,
-    "BC" : 0.5
-}
+
+def add_edge(mat, i, j, weight):
+  
+    # Add an edge between two vertices
+    mat[i][j] = weight  # Graph is 
+    mat[j][i] = weight  # Undirected
+
+def display_matrix(mat):
+  
+    # Display the adjacency matrix
+    for row in mat:
+        print(" ".join(map(str, row)))  
+
+# Main function to run the program
+
+V = 4  # Number of vertices
+mat = [[0] * V for _ in range(V)]  
+
+# Add edges to the graph
+add_edge(mat, 0, 1,1)
+add_edge(mat, 0, 2,1)
+add_edge(mat, 1, 2,1)
+add_edge(mat, 2, 3,1)
+
+# Optionally, initialize matrix directly
+"""
+mat = [
+    [0, 1, 0, 0],
+    [1, 0, 1, 0],
+    [0, 1, 0, 1],
+    [0, 0, 1, 0]
+]
+"""
+
+# Display adjacency matrix
+print("Adjacency Matrix:")
+display_matrix(mat)
