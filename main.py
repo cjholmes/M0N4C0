77177ce86a15c00513eb3a2d99d739572d1e0a8e
cmakhilan <cm.akhilan@gmail.com>
2025-01-07 13:58:59 -0500

    dont remember just pushing changes previously made

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from collections import deque
 import random
 import time 
 
-start_time = time.time()
+start_time = time.perf_counter()
 
 
 currency = ["a","b","c","d"]
@@ -24,22 +24,46 @@ def trade_opt(adj):
     current_value = 0
     current_route = 0
     for i in range(1,len(currency)):
-        for j in range(len(currency)):
+        for j in range(len(currency)): #loops through possible second paths, if it returns to the 0th node then moves to next iteration
+            if j == 0:
+                current_route = (str(0) + str(i) + str(i) + str(j))
+                current_value = adj[0][i]*adj[i][j]
+                if current_value > optimal_value:
+                    optimal_value = current_value
+                    optimal_route = current_route
+                continue
             for k in range(len(currency)):
-                if j == 0:
-                    current_route = (str(0) + str(i) + str(i) + str(j))
-                    current_value = adj[0][i]*adj[i][j]
-                elif k == 0:
-                    current_route = (str(0) + str(i) + str(i) + str(j) + str(j) + str(k))
-                    current_value = adj[0][i]*adj[i][j]*adj[j][k]
-                else:
-                    current_route = (str(0) + str(i) + str(i) + str(j) + str(j) + str(k) + str(k) +str(0))
-                    current_value = adj[0][i]*adj[i][j]*adj[j][k]*adj[k][0]
+                current_route = (str(0) + str(i) + str(i) + str(j) + str(j) + str(k) + str(k) +str(0))
+                current_value = adj[0][i]*adj[i][j]*adj[j][k]*adj[k][0]
                 if current_value > optimal_value:
                     optimal_value = current_value
                     optimal_route = current_route
     return optimal_route,optimal_value
 
+
+def trade_opt_rec(adj):
+    def find_optimal_route(current_route,current_value, visited):
+        nonlocal optimal_value, optimal_route
+        last_node = current_route[-1]
+        
+        if len(current_route) > 1 and last_node == 0:
+            if current_value > optimal_value:
+                optimal_value = current_value
+                optimal_route = current_route
+            return
+        
+        for next_node in range(len(adj)):
+            if next_node not in visited or next_node == 0:
+                new_value = current_value * adj[last_node][next_node]
+                find_optimal_route(current_route + [next_node], new_value, visited | {next_node})
+    
+    optimal_value = 0
+    optimal_route = []
+    
+    for start_node in range(1, len(adj)):
+        find_optimal_route([0, start_node], adj[0][start_node], {0, start_node})
+    
+    return ''.join(map(str, optimal_route)), optimal_value
 # Main function to run the program
 
 V = len(currency)  # Number of vertices
@@ -48,7 +72,7 @@ adj = [[0] * V for i in range(V)]
 
 for j in range(1,len(currency)):
     for i in range(j,len(currency)):
-        add_edge(adj, i, i-j, random.random())
+        add_edge(adj, i, i-j, 1 + i)
 # Add edges to the graph
 
 
@@ -56,6 +80,6 @@ for j in range(1,len(currency)):
 # Display adjacency matrix
 print("Adjacency Matrix:")
 display_matrix(adj)
-print(trade_opt(adj))
-print("--- %s seconds ---" % (time.time() - start_time))
+print(trade_opt_rec(adj))
+print((time.perf_counter() - start_time))
 
